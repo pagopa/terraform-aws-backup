86f7aa09d81268f7e41e6a0dd7600a27a4be9ae0
Walter Traspadini <walter.traspadini@pagopa.it>
2023-08-09 15:19:00 +0200

    dummy (#5)

--- variables.tf
+++ variables.tf
@@ -9,7 +9,6 @@ variable "backup_rule" {
   description = "A rule object that specifies a scheduled task that is used to back up a selection of resources"
 }
 
-
 variable "tags" {
   type        = map(string)
   description = "A map of tags to assign to the resources"
@@ -22,23 +21,19 @@ variable "advanced_backup_setting" {
   default     = {}
 }
 
-
 variable "selection_tag" {
   type        = map(any)
   description = "Tag-based conditions used to specify a set of resources to assign to a backup plan."
 
 }
 
-
 variable "iam_role_arn" {
   type        = string
   description = "Service role to be used by AWS Backup, created externally"
-
 }
 
 variable "enable_vault_lock_governance" {
   type        = bool
   description = "A variable that allows to enable vault lock in governance mode"
   default     = false
-
 }
