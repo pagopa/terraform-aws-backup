16779c5c95748a3248addfca5a83d17d143f3970
uolter <walter.traspadini@pagopa.it>
2023-08-09 15:17:38 +0200

    dummy

--- outputs.tf
+++ outputs.tf
@@ -14,6 +14,6 @@ output "backup_plan_arn" {
 }
 
 output "backup_plan_version" {
-  value       = join("", aws_backup_plan.backup_plan.*.version)
+  value       = join(" ", aws_backup_plan.backup_plan.*.version)
   description = "Unique, randomly generated, Unicode, UTF-8 encoded string that serves as the version ID of the backup plan"
 }
